5fbb2695091a0c422f688dfca43f5fb0f67f9476
Adrien Waksberg <adrien.waksberg@doctolib.fr>
2017-02-23 14:05:22 +0100

    remove unused comment

--- cookbooks/lvm/attributes/default.rb
+++ cookbooks/lvm/attributes/default.rb
@@ -1,9 +1,3 @@
 
-
 default[:lvm] = {}
 default[:mount_new_path] = {}
-#default[:lvm][:physical_volumes] = {}
-#default[:lvm][:volume_groups] = {}
-#default[:lvm][:logical_volumes] = {}
-#default[:lvm][:lvm_cache] = {}
-#default[:lvm][:mount_existing_path] = {}
